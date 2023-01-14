5db76f40f6e5ab5554eee064489241f2ace2e578
Stephen Rice <goatwipe@gmail.com>
2023-01-14 17:13:36 +0000

    more questions. answers shuffled

--- starter/assets/js/logic.js
+++ starter/assets/js/logic.js
@@ -32,20 +32,44 @@ function askQuestion(){
     let question = chooseQuestion();
     showQuestion(question);
 }
-
+// chooseQuestion: pick a question from the quiz at random.
 function chooseQuestion() {
-    return quiz[0];
+    return quiz[getRandomIndex(quiz.length)];
 }
+// rnd: generate a random integer in the range 0 to length-1.
+function getRandomIndex(length) {
+    return Math.floor(Math.random()*(length));
+}
+// shuffle: shuffle the elements of an array 
+function shuffleArray(anArray) {
+    const iterations = anArray.length;
+    for (index=0;index<iterations;index++) {
+        let randomIndex = getRandomIndex(anArray.length);
+        console.log("index = " + index + " randomIndex = " + randomIndex);
+        [anArray[index], anArray[randomIndex]] = [anArray[randomIndex], anArray[index]];
+        console.log(anArray);
+    }
+    return anArray;
+}
+
+function mixAnswers(questionObject) {
+    answersArray = Object.values(questionObject).slice(questionObjectAnswerStartIndex, // start/end defined immediately before the quiz in questions.js
+                                                    questionObjectAnswerEndIndex);
+    console.log(answersArray);
+    return shuffleArray(answersArray);
+    }
+
  function showQuestion(question) {
     quizScreen.setAttribute("correctAnswer", question.answer);
-    console.log("quizScreen.value" + quizScreen.getAttribute("correctAnswer"));
     questionEl.textContent = question.question;
-    answerOne.textContent = question.answer;
-    answerTwo.textContent = question.dummy1;
-    answerThree.textContent = question.dummy2;
-    answerFour.textContent = question.dummy3;
+    answerArray = mixAnswers(question);
+    answerOne.textContent = answerArray[0];
+    answerTwo.textContent = answerArray[1];
+    answerThree.textContent = answerArray[2];
+    answerFour.textContent = answerArray[3];
  }
 
  function checkAnswer(answer) {
     return (quizScreen.getAttribute("correctAnswer") === answer);
  }
+ 
--- starter/assets/js/questions.js
+++ starter/assets/js/questions.js
@@ -1,20 +1,37 @@
-codeQuestions = [
-    {   question: "What is it?",
-        answer: "It's it.",
-        dummy1: "Say again?",
-        dummy2: "Who knows?",
-        dummy3: "Why not?"
+// conversion of a set of question object answers to an array...
+// requires knowing the start end indicies in all their majesty.
+const questionObjectAnswerStartIndex = 1;
+const questionObjectAnswerEndIndex = 5;
+// the quiz is an array of question objects.
+const codeQuestions = [
+    {   question: "Naming convention: Which is best practice when naming JS variables?",
+        answer: "camelCase",
+        dummy1: "UPPERCASE",
+        dummy2: "PascalCase",
+        dummy3: "lowercase"
     },
-    {   question: "Why is it?",
-        answer: "Please leave!",
-        dummy1: "Stand up.",
-        dummy2: "Sit down.",
-        dummy3: "Back in your basket."
+    {   question: "Naming convention: Which is best practice when naming a JS constant?",
+        answer: "UPPERCASE",
+        dummy1: "camelCase",
+        dummy2: "PascalCase",
+        dummy3: "lowercase"
     },
-    {   question: "Why is it?",
-        answer: "Please leave!",
-        dummy1: "Stand up.",
-        dummy2: "Sit down.",
-        dummy3: "Back in your basket."
+    {   question: "Naming convention: Which is the most suitable Boolean variable name?",
+        answer: "isUser",
+        dummy1: "User",
+        dummy2: "getUser",
+        dummy3: "beUser"
+    },
+    {   question: "JavaScript language: Which is not a JavaScript keyword?",
+        answer: "method",
+        dummy1: "yield",
+        dummy2: "public",  
+        dummy3: "implements"
+    }, 
+    {   question: "JavaScript language: Which is not a JavaScript statement?",
+        answer: "public",
+        dummy1: "yield",
+        dummy2: "case",
+        dummy3: "var"
     }
 ]
